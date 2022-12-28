97d243c2361ef110e72c2d4cd00da455790c5a90
agodse21 <agodse9@gmail.com>
2022-12-28 21:59:40 +0530

    Node-exp-middelewar

--- Backend-Revision/Day-1/index.js
+++ Backend-Revision/Day-1/index.js
@@ -8,7 +8,7 @@ const timeLogger = (res, req, next) => {
   const endTime = new Date().getTime();
   console.log(endTime - startTime);
 };
-app.use(timeLogger)
+app.use(timeLogger);
 app.get("/", (req, res) => {
   const data = fs.readFileSync("./db.json", "utf-8");
   let parsed_data = JSON.parse(data);
