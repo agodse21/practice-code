906e52c0f4faa7863be00bd0c4230066664d68de
agodse21 <agodse9@gmail.com>
2023-01-03 22:55:09 +0530

    added

---
+++ Practice/test.js
@@ -0,0 +1,9 @@
+let arr=[40,13,15,5,18,2,90,95];
+
+// let min=Infinity;
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]<min){
+//         min=arr[i]
+//     }
+// }
+console.log(Math.min.apply(1,arr))
