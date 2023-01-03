45d0e93823d1f5712dc8268ba53ba4d54bdcebda
agodse21 <agodse9@gmail.com>
2023-01-03 16:13:58 +0530

    Code Added

--- Nextjs/next-js-testingng/pages/products/[id].jsx
+++ Nextjs/next-js-testingng/pages/products/[id].jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/router";
+const URL = "https://jsonplaceholder.typicode.com/users";
 export default function productsDetials() {
   const { id } = useRouter().query;
-
+  console.log(id)
+  useEffect(() => {
+    fetch(`${URL}/${id}`)
+      .then((data) => data.json())
+      .then((res) => {
+        console.log(id,res);
+      });
+  }, [id]);
   return <div>product Details{id}</div>;
 }
