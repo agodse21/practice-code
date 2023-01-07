a3b17eb9611b84b27fc1ab997da3b010e8f252d6
agodse21 <agodse9@gmail.com>
2023-01-07 17:10:09 +0530

    data

---
+++ signin-btn/src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [pass, setPass] = useState("");
+  return (
+    <div className="App">
+      <input placeholder="name" onChange={(e) => setName(e.target.value)} />
+      <input placeholder="pass" onChange={(e) => setPass(e.target.value)} />
+      <br />
+      <button
+        style={
+          name == "" || pass == ""
+            ? { background: "red" }
+            : { background: "yellow"}
+        }
+      >
+        submit
+      </button>
+    </div>
+  );
+}
+
+export default App;
