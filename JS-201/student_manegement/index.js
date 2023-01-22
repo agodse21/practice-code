528498b83f46394014dc5d07afa2e524e77b27d4
agodse21 <agodse9@gmail.com>
2023-01-22 22:01:06 +0530

    Addd

---
+++ JS-201/student_manegement/index.js
@@ -0,0 +1,26 @@
+let admin =()=>{
+window.location.href="./admin-signup.html"
+}
+
+
+
+
+let student=()=>{
+   window.location.href="./student-sign.html"
+//    C:\Users\Asus\Desktop\masai\js 201\sprit 3\day 3\student manegement\studentPages\student-sign.html
+}
+
+
+// let append=()=>{
+//     let main=document.getElementById("log_nd_sign");
+//     let form=document.createElement("form");
+//     let name=document.createElement("input");
+//     name.setAttribute("type","name")
+// name.setAttribute("id","name");
+// name.setAttribute("placeholder","Your Name");
+
+// let 
+// form.append(name)
+
+//     main.append(form)
+// }
