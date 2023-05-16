f05dc23cab970403d3f0c84a89ee54de96c5926a
agodse21 <agodse9@gmail.com>
2023-05-16 21:30:08 +0530

    added

---
+++ newNote.js
@@ -0,0 +1,18 @@
+// add js part for newNote page here
+let data = JSON.parse(localStorage.getItem("notes")) || [];
+
+let total_notes = document.getElementById("total_notes"); // to add no of notes
+total_notes.textContent = data.length;
+
+let btn = document.getElementById("add_note");
+btn.addEventListener("click", function () {
+  let title = document.getElementById("new_title").value;
+  let desc = document.getElementById("new_description").value;
+  const note = {
+    title: title,
+    description: desc,
+  };
+  data.push(note);
+  localStorage.setItem("notes", JSON.stringify(data));
+  window.location.reload();
+});
