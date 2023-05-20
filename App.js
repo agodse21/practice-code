c3112d50d6e510fe32e6d439180918db7e2d4f3e
Amol Godse <103682371+agodse21@users.noreply.github.com>
2023-05-20 10:02:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import { Box } from "@mui/system";
+
+import "./App.css";
+
+import { AllRoutes } from "./Routes/AllRoutes";
+
+function App() {
+  return (
+    <Box>
+      <AllRoutes />
+    </Box>
+  );
+}
+
+export default App;
