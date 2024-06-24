65d0d91e137204164fe1426cfb3904c9e3fda4da
Amol Godse <103682371+agodse21@users.noreply.github.com>
2024-06-24 22:34:33 +0530

    home page

---
+++ Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useTrackedState } from "../Reducer/store";
+
+export const Home = () => {
+  const navigate = useNavigate();
+  const state = useTrackedState();
+
+  if (!state.isAuth) {
+    navigate("/login");
+  }
+  return <div>Home</div>;
+};
