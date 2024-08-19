d123615bd7e68626844a4dfde94f170b7ba02234
Amol Godse <103682371+agodse21@users.noreply.github.com>
2024-08-19 22:02:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '../App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Router} from "react-router-dom
+import {Router,Route} from "react-router-dom
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
