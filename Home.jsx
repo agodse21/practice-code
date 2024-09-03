42a1ed663cc6bfccf0d82a61676b5427d27879c2
Amol Godse <103682371+agodse21@users.noreply.github.com>
2024-09-03 21:58:58 +0530

    Update Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useTrackedState } from "../Reducer/store";
-import "./app.css";
+import "./index.css";
 
 export const Home = () => {
   const navigate = useNavigate();
