777649654370b2324dccdf3cd362b7ad5a48e88a
Amol Godse <103682371+agodse21@users.noreply.github.com>
2024-11-23 22:09:51 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { Box } from "@mui/system";
 import {react} from "react";
 import "./app.css";
-import {BrowserRouter} from "react-router-dom"
+import {BrowserRouter,Router} from "react-router-dom"
 import { AllRoutes } from "./Routes/AllRoutes";
 
 function App() {
