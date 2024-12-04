4c3e3b2413a6b9e9426dd4f5bc7667c67d93d7db
Amol Godse <103682371+agodse21@users.noreply.github.com>
2024-12-04 23:07:03 +0530

    Update test.js

--- Practice/test.js
+++ Practice/test.js
@@ -3,7 +3,7 @@ let arr=[40,13,15,5,18,2,95];
 // let min=Infinity;
 // for(let i=0;i<arr.length;i++){
 //     if(arr[i]<min){
-//         min=arr[i]
-//     }
-// }
+      min=arr[i]
+    }
+ }
 console.log(Math.min.apply(1,arr))
