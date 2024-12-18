0bfdc90edcca690d4aada2f8a084e2b284a89f94
Amol Godse <103682371+agodse21@users.noreply.github.com>
2024-12-18 22:13:18 +0530

    Update index.js

--- IOB company/index.js
+++ IOB company/index.js
@@ -1,4 +1,4 @@
-const form = document.getElementById('form');
+const form = document.getElementById('form')?? null;
 const solutionDiv = document.getElementById('solution');
 let blocks;
 form.addEventListener('submit', (e) => {
